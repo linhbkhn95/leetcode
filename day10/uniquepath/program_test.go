f64777ec3e1b3116b253e7622e6f879ba57cdc07
linhbkhn95 <linhxuancb9596@gmail.com>
2021-05-26 22:20:14 +0700

    up code

---
+++ day10/uniquepath/program_test.go
@@ -0,0 +1,32 @@
+package uniquepath
+
+import "testing"
+
+func Test_uniquePaths(t *testing.T) {
+	type args struct {
+		m int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{
+				m: 51,
+				n: 9,
+			},
+			want: 1916797311,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := uniquePaths(tt.args.m, tt.args.n); got != tt.want {
+				t.Errorf("uniquePaths() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
