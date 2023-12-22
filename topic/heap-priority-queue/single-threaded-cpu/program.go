ba091ac36f0a819ee53b065b1b9cc97433badd14
kevin <kevin@gmail.com>
2023-12-22 15:06:32 +0700

    update change

---
+++ topic/heap-priority-queue/single-threaded-cpu/program.go
@@ -0,0 +1,91 @@
+package singlethreadedcpu
+
+import (
+	"container/heap"
+	"sort"
+)
+
+func getOrder(tasks [][]int) []int {
+	l := len(tasks)
+	availableTasks := make([]*AvaibleTask, len(tasks))
+	for i, task := range tasks {
+		availableTasks[i] = &AvaibleTask{
+			Index:          i,
+			ProcessingTime: task[1],
+			EnqueueTime:    task[0],
+		}
+	}
+	sort.Slice(availableTasks, func(i, j int) bool {
+		if availableTasks[i].EnqueueTime == availableTasks[j].EnqueueTime {
+			return availableTasks[i].ProcessingTime < availableTasks[j].ProcessingTime
+		}
+		return availableTasks[i].EnqueueTime < availableTasks[j].EnqueueTime
+	})
+	result := make([]int, 0, len(tasks))
+	result = append(result, availableTasks[0].Index)
+	endTime := availableTasks[0].ProcessingTime + availableTasks[0].EnqueueTime
+	i := 1
+	taskQueue := &AvailableTaskQueue{}
+	for {
+		if i == l {
+			break
+		}
+		for i < l && availableTasks[i].EnqueueTime <= endTime {
+			heap.Push(taskQueue, availableTasks[i])
+			i++
+		}
+		candicate := heap.Pop(taskQueue)
+		availTask := candicate.(*AvaibleTask)
+		result = append(result, availTask.Index)
+		endTime = endTime + availTask.ProcessingTime
+
+	}
+	for taskQueue.Len() > 0 {
+
+		candicate := heap.Pop(taskQueue)
+		availTask := candicate.(*AvaibleTask)
+		result = append(result, availTask.Index)
+	}
+	return result
+}
+
+type AvaibleTask struct {
+	Index          int
+	ProcessingTime int
+	EnqueueTime    int
+}
+
+type AvailableTaskQueue []*AvaibleTask
+
+// Len implements heap.Interface.
+func (a AvailableTaskQueue) Len() int {
+	return len(a)
+}
+
+// Less implements heap.Interface.
+func (a AvailableTaskQueue) Less(i int, j int) bool {
+	if a[i].ProcessingTime == a[j].ProcessingTime {
+		return a[i].Index < a[j].Index
+	}
+	return a[i].ProcessingTime < a[j].ProcessingTime
+}
+
+// Pop implements heap.Interface.
+func (a *AvailableTaskQueue) Pop() any {
+
+	item := (*a)[a.Len()-1]
+	(*a) = (*a)[:a.Len()-1]
+	return item
+}
+
+// Push implements heap.Interface.
+func (a *AvailableTaskQueue) Push(x any) {
+	*a = append(*a, x.(*AvaibleTask))
+}
+
+// Swap implements heap.Interface.
+func (a AvailableTaskQueue) Swap(i int, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+var _ heap.Interface = (*AvailableTaskQueue)(nil)
---
+++ topic/heap-priority-queue/single-threaded-cpu/program_test.go
@@ -0,0 +1,43 @@
+package singlethreadedcpu
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_getOrder(t *testing.T) {
+	type args struct {
+		tasks [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// {
+		// 	name: "",
+		// 	args: args{
+		// 		tasks: [][]int{
+		// 			{19, 13}, {16, 9}, {21, 10}, {32, 25}, {37, 4}, {49, 24}, {2, 15}, {38, 41}, {37, 34}, {33, 6}, {45, 4}, {18, 18}, {46, 39}, {12, 24},
+		// 		},
+		// 	},
+		// 	want: []int{6, 1, 2, 9, 4, 10, 0, 11, 5, 13, 3, 8, 12, 7},
+		// },
+		{
+			name: "",
+			args: args{
+				tasks: [][]int{
+					{5, 2}, {7, 2}, {9, 4}, {6, 3}, {5, 10}, {1, 1},
+				},
+			},
+			want: []int{4, 3, 2, 0, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getOrder(tt.args.tasks); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getOrder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
